0663549daf5b63d195ac01d18b8486afd855fab1
thanhNha <thanhna.k13@gmail.com>
2023-12-05 16:00:29 +0700

    thu vien dnd-kit keo tha column

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -2,21 +2,78 @@ import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
 
+import {
+  DndContext,
+  MouseSensor,
+  TouchSensor,
+  PointerSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
+import { useEffect, useState } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
+
 const BoardContent = ({ board }) => {
-  const orderColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  // https://docs.dndkit.com/api-documentation/sensors
+  const pointerSensor = useSensor(PointerSensor, {
+    activationConstraint: { distance: 10 }
+  })
+  // Yêu cầu chuột di chuyển 10px thì mới chạy hàm handleDragEnd tránh trường hợp user Click column
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: { distance: 10 }
+  })
+
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: { delay: 250, tolerance: 5 }
+  })
+  // const sensors = useSensors(pointerSensor)
+  const sensors = useSensors(mouseSensor, touchSensor)
+
+  const [orderColumnsState, setOrderColumnsState] = useState([])
+
+  useEffect(() => {
+    const orderColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+    setOrderColumnsState(orderColumns)
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    console.log('handleDragEnd: ', event)
+    const { active, over } = event
+
+    // Kiểm tra nếu không tồn tại over (Kéo ra ngoài )
+    if (!over) return
+    // Nếu vị trí sau khi kéo thả khác với vị trí ban đầu
+    if (active.id !== over.id) {
+      //Lấy vị cũ từ (active)
+      const oldIndex = orderColumnsState.findIndex((c) => c._id === active.id)
+      //Lấy vị mới từ (active)
+      const newIndex = orderColumnsState.findIndex((c) => c._id === over.id)
+
+      //Dúng arrayMove của thằng dnd-kit để sắp xép lại mảng Columns ban đầu
+      // Code của arayMove ở đây: DOC nha (dnd--kit/packages/sorttable/src/utilities/arrayMove.ts)
+      const dndOrderColumns = arrayMove(orderColumnsState, oldIndex, newIndex)
+      // tạo ra arr để sort column theo mảng này. rồi updata trên DB
+      // const dndOrderColumnsIds = dndOrderColumns.map((c) => c._id)
+
+      // Cập nhật lại state để re-render
+      setOrderColumnsState(dndOrderColumns)
+    }
+  }
   return (
-    <Box
-      sx={{
-        bgcolor: (theme) =>
-          theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
-        width: '100%',
-        height: (theme) => theme.trello.boardContentHeight,
-        p: '10px 0'
-      }}
-    >
-      {/* set scrol */}
-      <ListColumns columns={orderColumns}></ListColumns>
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box
+        sx={{
+          bgcolor: (theme) =>
+            theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
+          width: '100%',
+          height: (theme) => theme.trello.boardContentHeight,
+          p: '10px 0'
+        }}
+      >
+        {/* set scrol */}
+        <ListColumns columns={orderColumnsState}></ListColumns>
+      </Box>
+    </DndContext>
   )
 }
 
